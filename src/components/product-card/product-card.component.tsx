2cdf67c37dec0dd86738d873263bfe2c83e4c9fb
aliev008 <hfe.as@yandex.ru>
2022-12-11 00:53:27 +0300

    fix: minor syntax changes

--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -3,19 +3,19 @@ import Button from "../button/button.component";
 import "./product-card.styles.scss";
 
 interface ProductCardProps {
-    product: any;
+  product: any;
 }
 
-export const ProductCard = ({product}: ProductCardProps) => {
-    const {id, name, imageUrl, price} = product;
+export const ProductCard = ({ product }: ProductCardProps) => {
+  const { name, imageUrl, price } = product;
   return (
-    <div className={`product-card-container`} >
-        <img src={imageUrl} alt={name} />
-        <div className='footer'>
-            <span className="name">{name}</span> 
-            <span className="price">{price}</span>
-        </div>
-        <Button buttonType="inverted">ADD TO CARD</Button>  
+    <div className="product-card-container">
+      <img src={imageUrl} alt={name} />
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price">{price} $</span>
+      </div>
+      <Button buttonType="inverted">ADD TO CARD</Button>
     </div>
-  )
-}
+  );
+};
--- src/routes/shop/shop.components.tsx
+++ src/routes/shop/shop.components.tsx
@@ -3,15 +3,15 @@ import { ProductsContext } from "../../contexts/products.context";
 
 import { ProductCard } from "../../components/product-card/product-card.component";
 
-import './shop.styles.scss';
+import "./shop.styles.scss";
 
 export const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div className='products-container'>
+    <div className="products-container">
       {products &&
         (products as any).map((product: any) => {
-          return <ProductCard key={ product.id } product={ product } />;
+          return <ProductCard key={product.id} product={product} />;
         })}
     </div>
   );
