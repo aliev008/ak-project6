ec25f44db5f12af5053826133914cb1fa5b09151
aliev008 <hfe.as@yandex.ru>
2022-12-18 15:52:05 +0300

    fix: added redirect by click on title on the categories preview route

--- src/components/category-preview/category-preview.component.tsx
+++ src/components/category-preview/category-preview.component.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { ProductCard } from "../product-card/product-card.component";
 
 import {
@@ -7,10 +8,18 @@ import {
 import "./category-preview.styles.scss";
 
 export const CategoryPreview = ({ title, products }: CategoryInterface) => {
+  const navigate = useNavigate();
+
+  const goToCategoryHandler = () => {
+    navigate(`/shop/${title}`);
+  };
+
   return (
     <div className="category-preview-container">
       <h2>
-        <span className="title">{title.toUpperCase()}</span>
+        <span className="title" onClick={goToCategoryHandler}>
+          {title.toUpperCase()}
+        </span>
       </h2>
       <div className="preview">
         {products &&
