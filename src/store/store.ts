deaad6a637ebe897796d13e02355818369a8e121
aliev008 <hfe.as@yandex.ru>
2023-01-07 19:11:58 +0300

    feat: added basic setup

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga"; 
 import { rootReducer } from "./root-reducer";
 import { persistStore, persistReducer} from 'redux-persist'; 
 import storage from 'redux-persist/lib/storage';
 import logger from "redux-logger";
-import thunk from 'redux-thunk';
 
 const persistConfig = {
     key: 'root',
@@ -12,11 +13,15 @@ const persistConfig = {
 
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middlewares: any = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
+const middlewares: any = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 export const persistor = persistStore(store);
