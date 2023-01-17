ad24d9412840ff4398a6317e5381cc53021b36cc
aliev008 <hfe.as@yandex.ru>
2023-01-17 11:49:30 +0300

    feat: finished payment by card feature logic

--- src/components/payment-form/payment-form.component.tsx
+++ src/components/payment-form/payment-form.component.tsx
@@ -6,16 +6,45 @@ export const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
 
-  const paymenHandler = async (event: any) => {
+  const paymentHandler = async (event: any) => {
     event.preventDefault();
 
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((response) => response.json());
+
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "Farid Aliev",
+        },
+      },
+    } as any);
+
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert(`Payment successfull`);
+      }
+    }
   };
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit card payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_CLASS_TYPES.inverted}> Pay now </Button>
