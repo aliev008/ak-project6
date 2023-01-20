f06cdb2c78fb269977110adadde5d2755c43dee7
aliev008 <hfe.as@yandex.ru>
2023-01-20 15:14:10 +0300

    feat: typed category selectors

--- src/store/category/category.selector.ts
+++ src/store/category/category.selector.ts
@@ -1,7 +1,8 @@
-import { CategoryInterface } from "../../interfaces/interfaces";
 import { createSelector } from "reselect";
+import { CategoriesState } from "./category.reducer";
+import { CategoryMap } from "./category.types";
 
-const selectCategoryReducer = (state: any) => state.categories;
+const selectCategoryReducer = (state: any): CategoriesState => state.categories;
 
 const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -10,12 +11,12 @@ const selectCategories = createSelector(
 
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
-    categories.reduce((acc: any, category: CategoryInterface) => {
+  (categories): CategoryMap =>
+    categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectIsLoading = createSelector(
